f43195df6ac02bd6b09cedd087ec2184796e86f2
HRD <HRD@DESKTOP-MIFHSTI>
2023-05-30 11:18:19 +0900

    기본설정 완료

---
+++ src/main/java/com/jjcompany/jpaBoard/TeamProject230530JjApplication.java
@@ -0,0 +1,13 @@
+package com.jjcompany.jpaBoard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamProject230530JjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamProject230530JjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jjcompany/jpaBoard/TeamProject230530JjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jjcompany.jpaBoard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamProject230530JjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
